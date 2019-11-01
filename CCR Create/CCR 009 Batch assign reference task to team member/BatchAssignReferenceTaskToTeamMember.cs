037affa6e7db21543e6df539a3f1b2f965474bee
Peter Meurer <31315727+PeterMeurer@users.noreply.github.com>
2019-11-01 13:09:53 +0100

    Create BatchAssignReferenceTaskToTeamMember.cs

---
+++ CCR Create/CCR 009 Batch assign reference task to team member/BatchAssignReferenceTaskToTeamMember.cs
@@ -0,0 +1,53 @@
+//CCR 009 Batch Assign Reference Task To Team Member
+using System;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Metadata;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Collections;
+
+
+public static class CitaviMacro
+{
+	public static void Main()
+	{		
+		if (Program.ActiveProjectShell == null) return;
+		Project activeProject = Program.ActiveProjectShell.Project;
+		if (activeProject == null)
+		{
+			DebugMacro.WriteLine("No active project.");
+			return;
+		}
+		List<Reference> references = Program.ActiveProjectShell.PrimaryMainForm.GetFilteredReferences();
+		if (references == null || !references.Any())
+		{
+			DebugMacro.WriteLine("No references selected.");
+			return;
+		}
+		
+		ContactInfo taskAssignee = activeProject.Contacts.GetMembers().FirstOrDefault<ContactInfo>(item => item.FullName.Equals("Peter Meurer"));
+		if (taskAssignee == null) 
+		{
+			DebugMacro.WriteLine("No such contact.");
+			return;
+		}			
+		DateTime? dueDate = new DateTime(2019, 12, 31);
+		KnownTaskName taskName = KnownTaskName.ExcerptQuotations;
+			
+		
+		foreach (Reference reference in references)
+		{
+			//reference.Tasks.Clear();
+		    TaskItem task = reference.Tasks.Add(taskName);
+		    task.AssignedTo = taskAssignee.Key;
+			task.DueDate = dueDate;
+		}
+	
+	
+		DebugMacro.WriteLine("Done.");
+	}
+}
